e7d6c393b6c01e841b8c6f354454fc2aab93a339
SatriaSuprapto <32392756+SatriaSuprapto@users.noreply.github.com>
2018-01-19 15:49:24 +0700

    meulah class pinguin

---
+++ pinguin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pinguin{
+	public:
+		void warna(string warna);
+		double beratdewasa;
+		string ukuran;
+		string nama;	
+};
+
+int main(){
+	
+	
+}
